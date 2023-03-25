cda790ca053163cdbd913c059766b4abde818988
bhsimon97 <76971018+bhsimon97@users.noreply.github.com>
2023-03-25 11:38:16 -0400

    login form JSX and components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import{
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import LoginPage from './pages/LoginPage';
+
+// const apiKey = process.env.REACT_APP_API_KEY
+// const apiUrl = 'https://frontend-take-home-service.fetch.com'
+
+// fetch(apiUrl + '/auth/login', {
+//   method: 'POST',
+//   headers: {
+//     'fetch-api-key' : apiKey,
+//     'Content-Type': 'application/json'
+//   },
+//   body: JSON.stringify({
+//     'name': 'userName',
+//     'email': 'userEmail@email.com'
+//   })
+// })
+// .then (resp => resp.json())
+// .then(function(data) {
+//   console.log(data)
+// })
+// .catch(function(error){
+//   console.log(error);
+// })
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-full h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full space-y-8">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<LoginPage/>} />
+          </Routes>
+        </BrowserRouter>
+      </div>
     </div>
   );
 }
---
+++ src/constants/formFields.js
@@ -0,0 +1,24 @@
+const loginFields = [
+    {
+        labelText:"Name",
+        labelFor:"name",
+        id:"name",
+        name:"name",
+        type:"text",
+        autocomplete:"name",
+        isRequired: true, 
+        placeholder:"Your name"
+    },
+    {
+        labelText:"Email",
+        labelFor:"email",
+        id:"email",
+        name:"email",
+        type:"email",
+        autoComplete:"email",
+        isRequired: true,
+        placeholder: "Email address"
+    }
+]
+
+export {loginFields}
